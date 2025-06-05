1d007c5b4d437ff55b26441597b4c096c72789bd
Rafał Gromulski <rgromulski@gmail.com>
2025-06-05 15:55:47 +0200

    13. Dockerfile's, entrypoint.sh - changed and added

---
+++ tsl_manager_project/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+set -e
+
+if [ -f .env ]; then
+  set -a
+  . .env || echo "Warning: Failed to load .env file"
+  set +a
+fi
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+    "${DJANGO_SUPERUSER_USERNAME}",
+    "${DJANGO_SUPERUSER_EMAIL}",
+    "${DJANGO_SUPERUSER_PASSWORD}"
+    )
+EOF
+
+exec gunicorn tsl_manager_project.wsgi:application --bind 0.0.0.0:8000
