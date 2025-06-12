e58e3093cdf94fdffad2d8741897320de7acfcbd
Rafał Gromulski <rgromulski@gmail.com>
2025-06-12 16:01:36 +0200

    20. e.g. settings.py - changed

--- tsl_manager_project/tsl_manager_project/settings.py
+++ tsl_manager_project/tsl_manager_project/settings.py
@@ -19,8 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    # "whitenoise.runserver_nostatic",
-    # "django.contrib.staticfiles",
+    "django.contrib.staticfiles",
     "django_bootstrap5",
     "django_filters",
     # "django_crontab",
@@ -30,7 +29,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
