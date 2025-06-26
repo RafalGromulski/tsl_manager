33cf71a3894ff82d8e2146132ddf73d3f55f278b
Rafał Gromulski <rgromulski@gmail.com>
2025-06-26 22:21:01 +0200

    30. docker-compose.yaml - changed

--- tsl_manager_downloader/celeryconfig.py
+++ tsl_manager_downloader/celeryconfig.py
@@ -14,14 +14,4 @@ beat_schedule: dict[str, dict[str, Any]] = {
         "schedule": crontab(minute=0, hour="*/2"),
         "options": {"queue": "tsl"},
     },
-    "run-tsl-task-at-specific-hours": {
-        "task": "tasks.update_all_tsl_task",
-        "schedule": crontab(minute=0, hour=[9, 15]),
-        "options": {"queue": "tsl"},
-    },
-    "run-tsl-task-daily": {
-        "task": "tasks.update_all_tsl_task",
-        "schedule": crontab(minute=45, hour=7),
-        "options": {"queue": "tsl"},
-    },
 }
